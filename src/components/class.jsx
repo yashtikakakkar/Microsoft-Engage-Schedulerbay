30c38d7ce982c24c3f22c098482308cb9f760b95
Yashtika Kakkar <yashtika2000@gmail.com>
2021-11-24 23:54:59 +0530

    Add files via upload

--- src/components/class.jsx
+++ src/components/class.jsx
@@ -2,8 +2,54 @@ import "./class.css";
 import logo from "./logo.png";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import firebase from "../firebase";
+import { useEffect, useState } from "react";
 
 let Class = () => {
+  const db = firebase.firestore();
+  let uid = "9WiD75L6R";
+
+  const [className, setClassName] = useState("");
+  const [type, setType] = useState("");
+  const [subject, setSubject] = useState("");
+  const [mon, setMon] = useState("");
+  const [tues, setTues] = useState("");
+  const [wed, setWed] = useState("");
+  const [thurs, setThurs] = useState("");
+  const [fri, setFri] = useState("");
+  const [sat, setSat] = useState("");
+
+  useEffect(() => {
+    db.collection("classes")
+      .get()
+      .then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          if (doc.id === uid) {
+            setClassName(doc.data().className);
+            setType(doc.data().classType);
+            setSubject(doc.data().subject);
+          }
+        });
+      });
+
+    db.collection("classes")
+      .doc(uid)
+      .collection("timetable")
+      .get()
+      .then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          if (doc.id === uid) {
+            setMon(doc.data().Monday);
+            setTues(doc.data().Tuesday);
+            setWed(doc.data().Wednesday);
+            setThurs(doc.data().Thursday);
+            setFri(doc.data().Friday);
+            setSat(doc.data().Saturday);
+          }
+        });
+      });
+  }, []);
+
   return (
     <>
       <div className="class-div">
@@ -17,7 +63,10 @@ let Class = () => {
             <hr />
           </div>
           <div className="class-content">
-            <h2>C L A S S &gt; IT-II</h2>
+            <h2>
+              C L A S S &gt; {className} - {subject} {type}
+            </h2>
+            <h6>Joining Code: {uid}</h6>
             <div className="class-inner">
               <div className="class-details">
                 <h4>TimeTable</h4>
@@ -28,27 +77,27 @@ let Class = () => {
                   </thead>
                   <tr>
                     <td>Monday</td>
-                    <td>10:00am - 11:00am</td>
+                    <td>{mon}</td>
                   </tr>
                   <tr>
                     <td>Tuesday</td>
-                    <td>10:00am - 11:00am</td>
+                    <td>{tues}</td>
                   </tr>
                   <tr>
                     <td>Wednesday</td>
-                    <td>No Class</td>
+                    <td>{wed}</td>
                   </tr>
                   <tr>
                     <td>Thursday</td>
-                    <td>10:00am - 11:00am</td>
+                    <td>{thurs}</td>
                   </tr>
                   <tr>
                     <td>Friday</td>
-                    <td>10:00am - 11:00am</td>
+                    <td>{fri}</td>
                   </tr>
                   <tr>
                     <td>Saturday</td>
-                    <td>No Class</td>
+                    <td>{sat}</td>
                   </tr>
                 </table>
               </div>
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,35 +1,59 @@
 import "./home.css";
 import logo from "./logo.png";
 import { signInWithGoogle, auth } from "../firebase";
-import { useEffect, useState,useContext } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { Redirect } from "react";
-import { authContext } from "../authProvider";
-
+import firebase from "../firebase";
 
 let Home = () => {
   const [role, setRole] = useState("");
-  
+  const db = firebase.firestore();
+  let flag = true;
+
   useEffect(() => {
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
       if (user) {
         console.log(user);
-        if (role=="teacher")
-          <Redirect to="/teacherDetails" />
-        if (role=="student")
-          <Redirect to="/studentDetails" />
+        let a = document.createElement("a");
+        if (role == "teacher") {
+          db.collection("teachers")
+            .get()
+            .then((snapshot) => {
+              snapshot.docs.forEach((doc) => {
+                if (doc.id === user.email) {
+                  a.href = "./teacher";
+                  a.click();
+                } else {
+                  a.href = "./teacherDetails";
+                }
+              });
+            });
+        }
+        if (role == "student")  {
+          db.collection("students")
+            .get()
+            .then((snapshot) => {
+              snapshot.docs.forEach((doc) => {
+                if (doc.id === user.email) {
+                  a.href = "./student";
+                  a.click();
+                } else {
+                  a.href = "./studentDetails";
+                }
+              });
+            });
+        }
+        a.click();
       }
     });
   });
-
 
   // let user = useContext(authContext);
 
   return (
     <>
-    {/* {user ? <Redirect to="/teacherDetails" /> : ""} */}
+      {/* {user ? <Redirect to="/teacherDetails" /> : ""} */}
       <div className="main-div">
         <div className="nav top">
           <div className="nav-content">
@@ -69,28 +93,28 @@ let Home = () => {
             <p>Now schedule your hybrid classes with ease</p>
             <div className="buttons">
               Join now as a
-                <button
-                  onClick={() => {
-                    signInWithGoogle();
-                    setRole("teacher");
-                  }}
-                  type="button"
-                  class="btn btn-outline-light"
-                >
-                  Teacher
-                </button>
+              <button
+                onClick={() => {
+                  signInWithGoogle();
+                  setRole("teacher");
+                }}
+                type="button"
+                class="btn btn-outline-light"
+              >
+                Teacher
+              </button>
               or a
-                <button
-                  onClick={() => {
-                    signInWithGoogle();
-                    setRole("student");
-                  }}
-                  type="button"
-                  class="btn btn-outline-light"
-                  style={{ marginRight: "0%" }}
-                >
-                  Student
-                </button>
+              <button
+                onClick={() => {
+                  signInWithGoogle();
+                  setRole("student");
+                }}
+                type="button"
+                class="btn btn-outline-light"
+                style={{ marginRight: "0%" }}
+              >
+                Student
+              </button>
             </div>
           </div>
         </div>
