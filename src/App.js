be9859e3e893fc5621e28b8bcbace4c915bf5234
Yashtika Kakkar <yashtika2000@gmail.com>
2021-11-27 16:08:01 +0530

    final ups

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../src/components/home";
-import TeacherSignup from "../src/components/teachersignup";
-import StudentSignup from "../src/components/studentsignup";
 import Thomepage from "./components/thomepage";
 import Class from "./components/class";
 import Shomepage from "./components/shomepage";
 import Subject from "./components/subject";
+import Tdetails from "./components/tdetails";
+import Sdetails from "./components/sdetails";
+import { useState } from "react";
+import { Context } from "./components/context";
 
 let App = () => {
+  const [tuid, setTuid] = useState("null");
+  const [suid, setSuid] = useState("null");
+
   return (
     <>
       {/* <Navbar /> */}
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="teachersignup/*" element={<TeacherSignup />} />
-          <Route path="studentsignup/*" element={<StudentSignup />} />
-          <Route path="teacher/*" element={<Thomepage />} />
-          <Route path="class/*" element={<Class />} />
-          <Route path="student/*" element={<Shomepage />} />
-          <Route path="subject/*" element={<Subject />} />
-        </Routes>
-      </Router>
+      <Context.Provider value={([tuid, setTuid], [suid, setSuid])}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="class/*" element={<Class />} />
+            <Route path="teacher/*" element={<Thomepage />} />
+            <Route path="student/*" element={<Shomepage />} />
+            <Route path="subject/*" element={<Subject />} />
+            <Route path="teacherDetails/*" element={<Tdetails />} />
+            <Route path="studentDetails/*" element={<Sdetails />} />
+          </Routes>
+        </Router>
+      </Context.Provider>
     </>
   );
 };
